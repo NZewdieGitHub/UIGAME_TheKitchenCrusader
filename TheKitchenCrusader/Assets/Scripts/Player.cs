8dcda24c488c86582a54675beda1b787bf504712
Nahum_Zewdie_GitHub <nzewdie@uccs.edu>
2023-12-06 19:49:44 -0700

    Made a bool for animations (WSIP)

--- TheKitchenCrusader/Assets/Scripts/Player.cs
+++ TheKitchenCrusader/Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 {
     // Animation fields
     public Animator animator;
-
+    public bool isMoving = false;
 
     // Player movement setup
     public Rigidbody2D rb2d;
@@ -37,6 +37,8 @@ public class Player : MonoBehaviour
         // Set player to idle
         animator = gameObject.AddComponent<Animator>();
         animator.SetFloat("MovementSpeed", 0f);
+        isMoving = false;
+
     }
 
     // Update is called once per frame
@@ -83,6 +85,7 @@ public class Player : MonoBehaviour
     {
         // Used for actual movement
         rb2d.MovePosition(rb2d.position + movement * playerSpeed * Time.fixedDeltaTime);
+
         // Update animation
         animator.SetFloat("MovementSpeed", playerSpeed);
     }
